7cff59c15e65c4bd0a7621928f209c9fb7bfda09
Kata <caua.ribeirow@gmail.com>
2023-03-31 19:48:09 -0300

    Exercicio Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//teste disso :)
+string Nome, Sobrenome;
+
+Console.WriteLine($"Digite Seu Nome.......: ");
+Nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu Sobrenome..: ");
+Sobrenome = Console.ReadLine();
+
+Console.WriteLine($"{Nome} {Sobrenome}");
+Console.WriteLine($"{Sobrenome.ToUpper()}, {Nome}");
